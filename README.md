a4d8ecf9dfb588209b348431acfb8b434657dfa4
Ossi Rönnberg <ossi.ronnberg@gmail.com>
2022-10-30 17:56:27 +0200

    Added firebase db write and invervals.

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Run the following command:
 ```sh
 sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 ```
+
+### ruuviTagsToRead.json example
+
+```
+{
+    "macs": [
+        "0123456789abc",
+        "9876543210def"
+    ]
+}
+``
--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-const ruuvi = require('node-ruuvitag');
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
+const ruuvi = require("node-ruuvitag");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+const ruuviTagsToRead = require("./ruuviTagsToRead.json");
+const interval = 900000; // 15mins in millisedoncs
+
+console.log("Starting RuuviTag reader for macs", ruuviTagsToRead.macs);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
-ruuvi.on('found', tag => {
-  console.log('Found RuuviTag, id: ' + tag.id);
-  tag.on('updated', data => {
-    console.log('Got data from RuuviTag ' + tag.id + ':\n' +
-      JSON.stringify(data, null, '\t'));
-  });
+const db = admin.firestore();
+
+ruuvi.on("found", (tag) => {
+  if (ruuviTagsToRead.macs.includes(tag.id)) {
+    console.log("Found RuuviTag, id: " + tag.id);
+    var lastUpdate = null;
+
+    tag.on("updated", (data) => {
+      if (lastUpdate === null || new Date() - lastUpdate > interval) {
+        console.log(
+          "Got data from RuuviTag " +
+            tag.id +
+            ":\n" +
+            JSON.stringify(data, null, "\t")
+        );
+        db.collection("datapoints").add({
+          dataFormat: data.dataFormat,
+          rssi: data.rssi,
+          temperature: data.temperature,
+          humidity: data.humidity,
+          pressure: data.pressure,
+          accelerationX: data.accelerationX,
+          accelerationY: data.accelerationY,
+          accelerationZ: data.accelerationZ,
+          battery: data.battery,
+          txPower: data.txPower,
+          movementCounter: data.movementCounter,
+          measurementSequenceNumber: data.measurementSequenceNumber,
+          mac: data.mac,
+          timestamp: admin.firestore.Timestamp.now(),
+        });
+        lastUpdate = Date.now();
+      } else {
+        console.log("Interval not reached, skipping update", tag.id);
+      }
+    });
+  }
 });
 
-ruuvi.on('warning', message => {
+ruuvi.on("warning", (message) => {
   console.error(new Error(message));
 });
